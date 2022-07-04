7566bb51529e0883f13f3bc4d53c9ae8f48bc809
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-07-04 16:00:07 +0200

    Aggiunto metodo old a create, esteso tutto l'esercizio ad update e view edit

--- crud/resources/views/comics/edit.blade.php
+++ crud/resources/views/comics/edit.blade.php
@@ -8,17 +8,45 @@
         @method('PUT')
 
         <div class="form-group">
-          <label for="title">Titolo</label>
-          <input type="text" class="form-control" name="title" id="title" placeholder="Inserisci il titolo">
+            <label for="title">Titolo</label>
+            <input type="text"
+                class="form-control @error('title') is-invalid @enderror"
+                name="title" id="title"
+                placeholder="Inserisci il titolo"
+                value="{{ old('title', $comic->title) }}"
+            >
+            @error('title')
+                <p class="error-msg">{{ $message }}</p>
+            @enderror
         </div>
+
         <div class="form-group">
-          <label for="image">Immagine</label>
-          <input type="text" class="form-control" name="image" id="image" placeholder="Inserisci l'URL immagine">
+            <label for="image">Immagine</label>
+            <input type="text"
+                class="form-control @error('image') is-invalid @enderror"
+                name="image" id="image"
+                placeholder="Inserisci l'URL immagine"
+                value="{{ old('image', $comic->image) }}"
+            >
+            @error('image')
+                <p class="error-msg">{{ $message }}</p>
+            @enderror
         </div>
+
         <div class="form-group">
-          <label for="type">Tipo</label>
-          <input type="text" class="form-control" name="type" id="type" placeholder="Inserisci il tipo di fumetto">
+            <label for="type">Tipo</label>
+            <input type="text"
+                class="form-control @error('type') is-invalid @enderror"
+                name="type" id="type"
+                placeholder="Inserisci il tipo di fumetto"
+                value="{{ old('type', $comic->type) }}"
+            >
+            @error('type')
+                <p class="error-msg">{{ $message }}</p>
+            @enderror
         </div>
+
         <button type="submit" class="btn btn-primary">Submit</button>
+
     </form>
 @endsection
